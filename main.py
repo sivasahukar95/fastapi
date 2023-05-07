014517710d6afe3007dd80fd62ff8c7600a3731c
Siva Santosh <siva.santosh@zs.com>
2023-05-07 18:20:57 +0530

    the first main.py

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ app = FastAPI()
 
 
 @app.get('/')
-async def root():
-    return {"message": "hello world"}
+async def base_get_route():
+    return {"message": "hello world, this is the get operation from the fastAPI"}
+
+@app.post('/', description= "this is the second route")
+async def base_post():
+    return {"message": "this is the post operation from the fast api"}
+
+@app.put('/{id}')
+async def put():
+    return {"message": "hello from the put route"}
